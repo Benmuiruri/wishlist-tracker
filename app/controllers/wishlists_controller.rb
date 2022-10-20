a6004b10023c80339d8462b99c39e215c40489a5
Benmuiruri <ben.muiruri13@gmail.com>
2022-10-20 19:05:23 +0300

    Add actions to wishlist controller

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,2 +1,49 @@
 class WishlistsController < ApplicationController
+  before_action :set_wishlist, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @wishlists = Wishlist.all
+  end
+
+  def show; end
+
+  def new
+    @wishlist = Wishlist.new
+  end
+
+  def edit; end
+
+  def create 
+    @wishlist = Wishlist.new(wishlist_params )
+
+    if @wishlist.save
+      redirect_to @wishlist, notice: "Wishlist was successfully created."
+    else 
+      render :new
+    end
+  end
+
+  def update
+    if @wishlist.update(wishlist_params)
+      redirect_to @wishlist, notice: "Wishlist was successfully updated."
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @wishlist.destroy
+    redirect_to wishlists_path, notice: "Wishlist was successfully destroyed."
+  end
+
+  private
+
+  def set_wishlist
+    @wishlist = Wishlist.find(params[:id])
+  end
+
+  def wishlist_params
+    params.require(:wishlist).permit(:name)
+  end
+  end
 end

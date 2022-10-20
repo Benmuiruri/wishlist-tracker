5c3ea0734725aedb09dac1e0854fce48c4d4c1fe
Benmuiruri <ben.muiruri13@gmail.com>
2022-10-20 18:08:56 +0300

    Add test for creating new wishlist

--- spec/features/wishlists_spec.rb
+++ spec/features/wishlists_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "Wishlists", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+ scenario "User creates a new wishlist" do
+    visit root_path
+    click_link "New Wishlist"
+    fill_in "Name", with: "My Wishlist"
+    click_button "Create Wishlist"
+    expect(page).to have_content("My Wishlist")
+  end
 end

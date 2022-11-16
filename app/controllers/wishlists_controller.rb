7463c6794d6b4010b1ecdeca59e5cfc2bbda0dab
Benmuiruri <ben.muiruri13@gmail.com>
2022-11-16 12:12:47 +0300

    respond to turbo_stream to allow turbo deleting

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,5 +1,5 @@
 class WishlistsController < ApplicationController
-  before_action :set_wishlist, only: [:show, :edit, :update, :destroy]
+  before_action :set_wishlist, only: %i[show edit update destroy]
 
   def index
     @wishlists = Wishlist.all
@@ -13,27 +13,30 @@ class WishlistsController < ApplicationController
 
   def edit; end
 
-  def create 
-    @wishlist = Wishlist.new(wishlist_params )
+  def create
+    @wishlist = Wishlist.new(wishlist_params)
 
     if @wishlist.save
-      redirect_to wishlists_path, notice: "Wishlist was successfully created."
-    else 
+      redirect_to wishlists_path, notice: 'Wishlist was successfully created.'
+    else
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
     if @wishlist.update(wishlist_params)
-      redirect_to wishlists_path, notice: "Wishlist was successfully updated."
-    else 
+      redirect_to wishlists_path, notice: 'Wishlist was successfully updated.'
+    else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @wishlist.destroy
-    redirect_to wishlists_path, notice: "Wishlist was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to wishlists_path, notice: 'Wishlist was successfully destroyed.' }
+      format.turbo_stream
+    end
   end
 
   private
